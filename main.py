60ea04ee468b3330d9bba8d411b2771090ce3166
AnishaChauhan <chauhananisha22@gmail.com>
2021-10-20 11:32:12 +0530

    main

---
+++ main.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import numpy as np
+a = tf.constant([[1, 2],
+                 [3, 4]])
+b = tf.constant([[1, 1],
+                 [1, 1]]) # Could have also said `tf.ones([2,2])`
+
+print(tf.add(a, b), "\n")
+print(tf.multiply(a, b), "\n")
+print(tf.matmul(a, b), "\n")
